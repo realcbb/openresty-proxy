f59a83f9e14e296cb5fe59f4a3e02c1c273e661b
realcbb <realcbb@gmail.com>
2020-12-10 23:38:44 +0800

    Update proxy_auth.lua
    set Connection header

--- proxy_auth.lua
+++ proxy_auth.lua
@@ -8,3 +8,4 @@ ngx.log(ngx.DEBUG, ngx.req.raw_header())
 
 -- transfer Proxy-Authorization header to Authorization for auth basic module
 ngx.req.set_header("Authorization", ngx.var.http_proxy_authorization)
+ngx.req.set_header("Connection", ngx.var.http_proxy_connection)
